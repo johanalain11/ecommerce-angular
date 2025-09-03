7a272f9218ac52e6a994a2080ea6dcb2ca7c41bd
johanalain11 <jank092016@gmail.com>
2025-09-03 15:11:41 +0100

    Mise à jour de la liste de produits pour récupérer les articles à partir d'une API en utilisant Axios

--- src/app/pages/products-list/products-list.ts
+++ src/app/pages/products-list/products-list.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { signal } from '@angular/core';
 import { Product } from '../../models/products';
 import { ProductCard } from '../../components/product-card/product-card';
+import axios from 'axios';
 
 @Component({
   selector: 'app-products-list',
@@ -10,27 +11,13 @@ import { ProductCard } from '../../components/product-card/product-card';
   styleUrl: './products-list.css'
 })
 export class ProductsList {
-  products = signal<Product[]>([
-    {
-      id: 1,
-      title: 'Product 1',
-      price: 100,
-      image: 'favicon.ico',
-      stock: 10
-    },
-    {
-      id: 2,
-      title: 'Product 2',
-      price: 200,
-      image: 'favicon.ico',
-      stock: 5
-    },
-    {
-      id: 3,
-      title: 'Product 3',
-      price: 300,
-      image: 'favicon.ico',
-      stock: 0
-    }
-  ]);
+
+  products = signal<Product[]>([]);
+
+  async ngOnInit(){
+    axios.get('https://fakestoreapi.com/products/')
+      .then(response => {
+        this.products.set(response.data);
+      });
+  }
 }
