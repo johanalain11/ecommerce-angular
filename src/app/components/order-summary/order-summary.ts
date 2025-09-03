6ce7bd54846d9cf3161e21eeccdf48598f92a248
johanalain11 <jank092016@gmail.com>
2025-09-03 14:30:47 +0100

    Implémentation de la fonctionnalité de résumé de commande

--- src/app/components/order-summary/order-summary.ts
+++ src/app/components/order-summary/order-summary.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { Cart } from '../../services/cart';
 
 @Component({
   selector: 'app-order-summary',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './order-summary.css'
 })
 export class OrderSummary {
+  cartService = inject(Cart);
 
+  total = computed(() => {
+    let total = 0;
+    for (const item of this.cartService.cart()) {
+      total += item.price;
+    }
+    return total;
+  })
 }
